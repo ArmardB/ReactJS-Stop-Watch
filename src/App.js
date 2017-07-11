68682cb43681f7cd102d0ba389f0a12bf0e00b1b
Armard Bellamy <Armard@Armards-MacBook.local>
2017-07-10 20:47:06 -0400

    Updated copy and fixed typo

--- src/App.js
+++ src/App.js
@@ -9,11 +9,8 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welome to Timer</h2>
+          <h2>Welcome to the Simple Timer</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
         <Timer start={Date.now()}/>
       </div>
     );
