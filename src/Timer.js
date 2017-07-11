9e652b31709d470f9cec96ecc23e7ba083e7323c
Armard Bellamy <Armard@Armards-MacBook.local>
2017-07-10 20:27:13 -0400

    Added basic styling to timer

--- src/Timer.js
+++ src/Timer.js
@@ -4,7 +4,9 @@ class Timer extends Component {
   render(){
     return(
       <div>
-
+        <p>You have been on this site since: </p><br />
+        <span>55.98</span>
+        <p>seconds</p>
       </div>
     );
   }
